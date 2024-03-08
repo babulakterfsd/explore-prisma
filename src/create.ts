79f8f1ad08b5ad59ef2fca5ec8453e799b54dd3d
Babul Akter <babulakterfsd@gmail.com>
2024-03-09 01:42:14 +0600

    user model added

--- src/create.ts
+++ src/create.ts
@@ -1,36 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, UserRole } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 const main = async () => {
-  //  const result = await prisma.post.create({
-  //     data: {
-  //         title: "second title",
-  //         author: "Babul Akter",
-  //         content: "Babul is learning Prisma and he is enjoying it.",
-  //     }
-  //  })
-  //  console.log(result)
-
-  //cretae many
-  const createMany = await prisma.post.createMany({
-    data: [
-      {
-        title: 'seventh title',
-        author: 'Babul Akter',
-        content: 'Babul is learning Prisma and he is enjoying it.',
-      },
-      {
-        title: 'eighth title',
-        author: 'Babul Akter',
-        content: 'Babul is learning Prisma and he is enjoying it.',
-      },
-      {
-        title: 'ninth title',
-        author: 'Babul Akter',
-        content: 'Babul is learning Prisma and he is enjoying it.',
-      },
-    ],
+  const createUser = await prisma.user.create({
+    data: {
+      username: 'babulakterfsd',
+      email: 'babulakterfsd@gmail.com',
+      role: UserRole.user,
+    },
   });
 };
 
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const main = async () => {
-//  const result = await prisma.post.create({
-//     data: {
-//         title: "second title",
-//         author: "Babul Akter",
-//         content: "Babul is learning Prisma and he is enjoying it.",  
-//     }
-//  })
-
-//  console.log(result)
-
-const getAllFromDB = await prisma.post.findMany();
-
-console.log(getAllFromDB);
-
-
-
-};
+const main = async () => {};
 
 main();
